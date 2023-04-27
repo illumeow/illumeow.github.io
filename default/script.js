8b92735be983618cfcde1ab955817a69c236ce04
illumeow <illumeow0223@gmail.com>
2023-04-27 11:59:40 +0800

    use bootstrap 5

--- default/script.js
+++ default/script.js
@@ -1,19 +1,5 @@
-function incrementValue() {
-    var inputField = document.getElementById("myInput");
-    var value = parseInt(inputField.value);
-    inputField.value = isNaN(value) ? 1 : value + 1;
-    multiplyByTen();
-}
-
-function decrementValue() {
-    var inputField = document.getElementById("myInput");
-    var value = parseInt(inputField.value);
-    inputField.value = isNaN(value) ? 1 : value - 1;
-    multiplyByTen();
-}
-
 function multiplyByTen() {
-    var inputField = document.getElementById("myInput");
+    var inputField = document.getElementById("number-input");
     var selectedValue = parseInt(inputField.value);
     var result = selectedValue * 10;
     document.getElementById("result").value = result;
