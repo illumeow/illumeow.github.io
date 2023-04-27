6b1e87067ced052fb22cd6619b71fbff187696d2
illumeow <illumeow0223@gmail.com>
2023-04-27 10:57:54 +0800

    charge dir

--- script.js
+++
@@ -1,6 +0,0 @@
-function multiplyByTen() {
-    var selectElement = document.getElementById("number-input");
-    var selectedValue = selectElement.value;
-    var result = selectedValue * 10;
-    document.getElementById("result").innerHTML = "Result: " + result;
- }  
